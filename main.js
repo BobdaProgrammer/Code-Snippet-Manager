ab49c40cbc305a3c87a7d34ef1594881d6a9bbf1
BobdaProgrammer <144843651+BobdaProgrammer@users.noreply.github.com>
2023-10-08 08:31:33 +0100

    The Code Snippets are created

---
+++ main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const fs = require('fs');
+
+let win;
+function createWindow () {
+    win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    })
+    win.webContents.openDevTools()
+    win.loadFile('index.html');
+}
+
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+ipcMain.on('new-bundle', (event, bundle) => {
+    console.log('New Bundle:', bundle);
+    lastBundle = bundle;
+    event.returnValue = `Received bundle with ${bundle.files.length} files.`; 
+})
+ipcMain.on('dropped-file', (event, arg) => {
+    console.log('Dropped File(s):', arg);
+    event.returnValue = `Received ${arg.length} paths.`; 
+    // Specify the path to the file you want to read
+const filePath = `${arg}`;
+
+// Use fs.readFile() to read the file
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+    // The data variable contains the file content as a string
+    console.log('File content:', data);
+win.loadFile('Code Snippets/index.html'); // Load your HTML file
+
+win.webContents.on('did-finish-load', () => {
+  // Send the data to the renderer process
+  win.webContents.send('data', data);
+});
+
+});
+
+})
